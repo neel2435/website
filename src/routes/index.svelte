a256109a9ddaa6ccd17fa01dda9435fb2915762b
neel2435 <neeldoshi2435@gmail.com>
2022-08-07 15:23:21 -0700

    typewriter

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,8 +6,12 @@
 
 <body>
 <div class = "index">
-    <h1>Home</h1>
-    <p>Aspiring Software Engineer</p>
+    <div class = "typewriterH">
+        <h1>Hey, I'm Neel</h1>
+    </div>  
+    <div class = "typewriterP">
+        <p>University of California, Riverside</p>
+    </div>
 </div>
 </body>
 
@@ -22,10 +26,43 @@
         justify-content: center;
         text-align: center;
         position: absolute;
-        top: 50%;
+        place-items: center;
+        top: 45%;
         left: 50%;
         transform: translate(-50%, -50%);
     }
+    
+    .typewriterH {
+        animation: typing 2s steps(22), blink .5s step-end infinite alternate;
+        white-space: nowrap;
+        overflow: hidden;
+        border-right: 3px solid;
+        font-family: monospace;
+        font-size: 2em;
+        border-style: solid;
+        border-color: red;
+    }
 
+    .typewriterP{
+        animation: typing 2s steps(22), blink .5s step-end infinite alternate;
+        white-space: nowrap;
+        overflow: hidden;
+        border-right: 3px solid;
+        font-family: monospace;
+        font-size: 2em;
+        border-style: solid;
+        border-color: red;
+    }
+
+    @keyframes typing {
+        from { width: 0 }
+        to { width: 100% }
+    }
+            
+    @keyframes blink {
+        50% {
+            border-color: transparent
+        }
+    }
 
 </style>
