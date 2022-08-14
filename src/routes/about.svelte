ce3f3d510143849bb40c366694adaf4173d50973
neel2435 <neeldoshi2435@gmail.com>
2022-08-13 21:27:41 -0700

    update about

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -3,16 +3,26 @@
 </script>
 
 <Titlebar />
+<body>
 <div class="about">
 <h1>About me</h1>
-<p>Welcome to my website! I'm an aspiring software engineer set to graduate with a degree in computer science in the spring of 2023.</p>
-<a href="/">Home</a>
+<p style="
+font-size:16px;
+padding:15px;
+margin:0px;">Welcome to my website! I'm an aspiring software engineer set to graduate with a degree in computer science in the spring of 2023.</p>
 </div>
+</body>
 
 <style>
-    .about {
+    body {
+        background:linear-gradient(rgba(110, 211, 144, 1), rgba(182, 218, 194, 1 ));
+        margin: 0;
+        padding: 0;
         text-align: center;
+    }
+
+    .about {
         display: block;
-        margin: 20px auto;
+        margin: 0px;
     }
 </style>
